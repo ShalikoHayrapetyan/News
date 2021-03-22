f175c025eb00f15c760d34c2490a349715fe9bca
Shaliko <h.shaliko@mail.ru>
2021-03-22 23:38:34 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDvmPrdgCaZQ1BqFiUpfaerO0JKeb7lQgk",
+  authDomain: "news-aca.firebaseapp.com",
+  projectId: "news-aca",
+  storageBucket: "news-aca.appspot.com",
+  messagingSenderId: "559724492296",
+  appId: "1:559724492296:web:1007aa320247ef66db83bd"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
