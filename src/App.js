c0db93110545f7e74fd6033526c6661444a66a74
Shaliko <h.shaliko@mail.ru>
2021-03-24 16:41:57 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
+import firebase from 'firebase';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDvmPrdgCaZQ1BqFiUpfaerO0JKeb7lQgk",
@@ -14,21 +16,8 @@ firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>My Empty Project</h1>
     </div>
   );
 }
