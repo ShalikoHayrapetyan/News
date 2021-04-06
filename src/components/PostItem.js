bb73bf3352f37c92541643be39d821db04ad7a08
Hayko <haiko2003@yandex.ru>
2021-04-07 00:06:50 +0400

    Create news details page

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -8,7 +8,6 @@ import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 import CommentIcon from '@material-ui/icons/Comment';
 
 const useStyles = makeStyles((theme) => ({
